a871f63e6289cf7369791d8c44b53ec752fa5e07
Abdul Arif <55670928+abdularif0705@users.noreply.github.com>
2020-11-30 17:36:03 -0500

    Update clientSevlet.java

--- src/java/clientSevlet.java
+++ src/java/clientSevlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author BukayoDan
+ * @author TimeTech Team
  */
 @WebServlet(urlPatterns = {"/clientSevlet"})
 public class clientSevlet extends HttpServlet {
